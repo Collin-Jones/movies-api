4b8a55459df2e881786e4f8660bd5d0766369b2f
Collin Jones <collin.g.jones34@gmail.com>
2022-04-04 16:25:23 -0500

    finished the first part and tried to understand it

--- src/main/java/data/Movie.java
+++ src/main/java/data/Movie.java
@@ -1,4 +1,88 @@
 package data;
 
 public class Movie {
+    private String title;
+    private Number rating;
+    private String poster;
+    private Number year;
+    private String genre;
+    private String director;
+    private String plot;
+    private String actors;
+    private Number id;
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Number getRating() {
+        return rating;
+    }
+
+    public void setRating(Number rating) {
+        this.rating = rating;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public Number getYear() {
+        return year;
+    }
+
+    public void setYear(Number year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public Number getId() {
+        return id;
+    }
+
+    public void setId(Number id) {
+        this.id = id;
+    }
 }
