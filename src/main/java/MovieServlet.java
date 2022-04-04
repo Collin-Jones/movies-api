3acf2d1b8b96b8733e8605a13e4f98573f867271
Collin Jones <collin.g.jones34@gmail.com>
2022-04-04 14:10:29 -0500

    I think I did the first part right

---
+++ src/main/java/MovieServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MovieServlet", urlPatterns = "/movies/*")
+
+public class MovieServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, world!</h1>");
+    }
+}
