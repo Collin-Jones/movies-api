a7bd82025262b33a3d26f74c8173decae8abc46b
Collin Jones <collin.g.jones34@gmail.com>
2022-04-06 14:49:39 -0500

    DAO code

--- src/main/java/data/MoviesDaoFactory.java
+++ src/main/java/data/MoviesDaoFactory.java
@@ -1,5 +1,22 @@
 package data;
 
+
 public class MoviesDaoFactory {
 
+    // private static Config config = new Config();
+
+    public enum DAOType {MYSQL, IN_MEMORY}
+
+    ; //Notice we have two values here
+
+    public static InMemoryMoviesDao getMoviesDao(DAOType daoType) {
+
+        switch (daoType) {
+            case IN_MEMORY: { //yet we have one switch case. We'll get to that!
+                return new InMemoryMoviesDao();
+            }
+        }
+        return null;
+    }
 }
+
